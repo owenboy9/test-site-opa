ded0b9a9ef43acb5d791342543a9e35b1912c42d
owenboy9 <owenkraus9@gmail.com>
2024-04-29 15:04:14 +0200

    just checkin'

--- cypress/specs/step_definitions/example.js
+++ cypress/specs/step_definitions/example.js
@@ -1,5 +1,5 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('that i am on the homepage', () => {
+Given('that i am on the very homiest page', () => {
   cy.visit("/")
 });
